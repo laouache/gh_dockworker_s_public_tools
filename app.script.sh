ce845b4cbb1f46d693ed8f33fff376a645143952
Arnault sur ks.tovar.fr <arnault.tovar@gmail.com>
2017-04-30 22:42:29 +0200

    ''

---
+++ app.script.sh
@@ -0,0 +1,76 @@
+#!/usr/bin/env bash
+#================================================================
+# HEADER
+#================================================================
+#% SYNOPSIS
+#+    ${SCRIPT_NAME} [-hv] [-o[file]] args ...
+#%
+#% DESCRIPTION
+#%    This is a script template
+#%    to start any good shell script.
+#%
+#% OPTIONS
+#%    -o [file], --output=[file]    Set log file (default=/dev/null)
+#%                                  use DEFAULT keyword to autoname file
+#%                                  The default value is /dev/null.
+#%    -t, --timelog                 Add timestamp to log ("+%y/%m/%d@%H:%M:%S")
+#%    -x, --ignorelock              Ignore if lock file exists
+#%    -h, --help                    Print this help
+#%    -v, --version                 Print script information
+#%
+#% EXAMPLES
+#%    ${SCRIPT_NAME} -o DEFAULT arg1 arg2
+#%
+#================================================================
+#- IMPLEMENTATION
+#-    version         ${SCRIPT_NAME} 0.0.1
+#-    author          dockworker
+#-    copyright       Copyright (c) https://gitlab.com/dockworker/s_public_tools/ 
+#-    license         GNU General Public License
+#-    script_id       516981
+#-
+#================================================================
+#  HISTORY
+#     SEE GIT
+# 
+#================================================================
+#  DEBUG OPTION
+#    set -n  # Uncomment to check your syntax, without execution.
+#    set -x  # Uncomment to debug this shell script
+#
+#================================================================
+# END_OF_HEADER
+#================================================================
+
+source app.header.sh
+
+  #== Check/Set arguments ==#
+  [[ $# -gt 2 ]] && error "${SCRIPT_NAME}: Too many arguments" && usage 1>&2 && exit 2
+
+  #== Main part ==#
+  #===============#
+{ trap 'kill -TERM ${$}; exit 99;' TERM
+
+info "${SCRIPT_NAME}: start $(date "+%y/%m/%d@%H:%M:%S") with process id ${EXEC_ID} by ${USER}@${HOSTNAME}:${PWD}"\
+    $( [[ ${flagOptLog} -eq 1 ]] && echo " (LOG: ${fileLog})" || echo " (NOLOG)" );
+
+#================================================================
+# START SCRIPT
+#================================================================
+  #== start your program here ==#
+
+exec_cmd "ls -lrt $*"
+#sleep 5
+
+  #== end   your program here ==#
+#================================================================
+# END SCRIPT
+#================================================================
+
+scriptfinish ; } 2>&1 | tee ${fileLog}
+
+  #== End ==#
+  #=========#
+rc_restore
+exit $rc
+
